2531a6fc72e544b36287d97437e014828e5804aa
akiyoshing <ak47jptype89@outlook.jp>
2025-06-03 23:13:54 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 const ringData = {
-    "ティファニー": [Add commentMore actions
-      { carat: 0.3, name: "クラシックソリティア", image: "img/tiffany1.jpg", desc: "伝統的なソリティア。" }
-    ],
-    "カルティエ": [
-      { carat: 0.3, name: "ラブリング", image: "img/cartier1.jpg", desc: "シンプルで上品なデザイン。" }
-    ],
-    "ロイヤルアッシャー": [
-      { carat: 0.3, name: "アッシャーA", image: "img/ra1.jpg", desc: "王室にも愛されるデザイン。" },
-      { carat: 0.3, name: "アッシャーB", image: "img/ra2.jpg", desc: "サイドに小粒ダイヤ入り。" },
-      { carat: 0.3, name: "アッシャーC", image: "img/ra3.jpg", desc: "華やかなアーム。" }
-    ],
-    "ラザール": [
-      { carat: 0.4, name: "ラザールA", image: "img/laz1.jpg", desc: "輝きが際立つ。" },
-      { carat: 0.4, name: "ラザールB", image: "img/laz2.jpg", desc: "現代的な曲線。" }
-      // 必要なら他も追加
-    ],
-    "ブリリアンス＋": [
-      { carat: 0.8, name: "B＋A", image: "img/bp1.jpg", desc: "大粒で迫力のある印象。" },
-      { carat: 0.8, name: "B＋B", image: "img/bp2.jpg", desc: "細身アームで大きさを引き立てる。" }
-    ],
-    "NextDiamond": [
-      { carat: 1.01, name: "NextA", image: "img/nd1.jpg", desc: "ラボグロウンの透明感。" },
-      { carat: 1.01, name: "NextB", image: "img/nd2.jpg", desc: "次世代の輝き。" }
-    ]
-  };
+  "ティファニー": [
+    { carat: 0.3, name: "クラシックソリティア", image: "img/tiffany1.jpg", desc: "伝統的なソリティア。" }
+  ],
+  "カルティエ": [
+    { carat: 0.3, name: "ソリテール", image: "img/cartier1.jpg", desc: "シンプルで上品なデザイン。" }
+  ],
+  "ロイヤルアッシャー": [
+    { carat: 0.3, name: "アッシャーカットA", image: "img/ra1.jpg", desc: "王室にも愛される王道デザイン。" },
+    { carat: 0.3, name: "アッシャーカットB", image: "img/ra2.jpg", desc: "サイドに小粒ダイヤ入り。" },
+    { carat: 0.3, name: "アッシャーカットC", image: "img/ra3.jpg", desc: "華やかなアーム。" }
+  ],
+  "ラザール": [
+    { carat: 0.4, name: "ユニオン", image: "img/laz1.jpg", desc: "輝きを最大限に引き出すデザインで、あふれる光を指先に纏って" },
+    { carat: 0.4, name: "トンプソン", image: "img/laz2.jpg", desc: "サイドのダイヤがメインのダイヤを輝かす" },
+    { carat: 0.4, name: "ローズランド", image: "img/laz3.jpg", desc: "5石の装飾で、華やかかつ優美なデザイン" },
+    { carat: 0.4, name: "アーヴィング", image: "img/laz4.jpg", desc: "端正でスタンダードな「約束」の証" }
+  ],
+  "ブリリアンス＋": [
+    { carat: 0.8, name: "B＋A", image: "img/bp1.jpg", desc: "大粒で迫力のある印象。" },
+    { carat: 0.8, name: "B＋B", image: "img/bp2.jpg", desc: "細身アームで大きさを引き立てる。" }
+  ],
+  "NextDiamond": [
+    { carat: 1.01, name: "NextA", image: "img/nd1.jpg", desc: "大きなダイヤが目を引く、シンプルなデザイン(注意：ラボダイヤ)" }
+  ]
+};
